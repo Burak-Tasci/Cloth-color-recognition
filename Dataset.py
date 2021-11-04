e2cbdf31ce5a4116a43795cec1cb608fc4e4eb58
Burak Tasci <buraaktascii@outlook.com>
2021-11-04 12:41:21 +0300

    Dataset.py updated
    added __getitem__ and __take_data functions

--- Dataset.py
+++ Dataset.py
@@ -11,7 +11,8 @@ class Dataset():
         self.length = sum(self.__imageCount().values())
         self.lengths_by_class = self.__imageCount()
         self.shape = tuple((self.length, *self.__shape()))
-        self.dataset = None
+        self = self.__take_data()
+        
         
     def __imageCount(self):
         """
@@ -25,16 +26,21 @@ class Dataset():
             images[subdir] += len(glob.glob(self.data_dir + "/" +subdir +"/*.jpeg"))
         return images
     
-    def __call__(self):  
-        image_pool = np.array()
+    def __take_data(self):  
+        image_pool = np.array([])
         for subdir in self.classes:
-            for path in glob.glob(self.data_dir + "/" +subdir +"/*.png") + \
-                        glob.glob(self.data_dir + "/" +subdir +"/*.jpg") + \
-                        glob.glob(self.data_dir + "/" +subdir +"/*.jpeg"):
+            print("Class "+subdir+"'s objects are creating.")
+            for i,path in enumerate(glob.glob(self.data_dir + "/" +subdir +"/*.png") + \
+                                    glob.glob(self.data_dir + "/" +subdir +"/*.jpg") + \
+                                    glob.glob(self.data_dir + "/" +subdir +"/*.jpeg")):
                 image = cv2.imread(path)
-                image_pool = np.append(image_pool, image).reshape(-1,image.shape)
-        self.dataset = image
+                image_pool = np.append(image_pool, image).reshape(-1,image.shape[0],image.shape[1],image.shape[2])
+                print(f"{subdir}: {(i/self.lengths_by_class[subdir])*100}")
+        print("*"*25)
         return image_pool
+
+    def __getitem__(self, item):
+         return getattr(self, item)
 
     def __shape(self):
         for subdir in self.classes:
