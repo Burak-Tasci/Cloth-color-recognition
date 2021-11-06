47ee255bc4832028b51f8ae165590d21a90b3721
Burak Tasci <buraaktascii@outlook.com>
2021-11-06 18:27:30 +0300

    Added BGR choose to Dataset class

--- Color.py
+++ Color.py
@@ -1,22 +1,19 @@
-import argparse
+import itertools
 from Dataset import Dataset
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import pandas as pd
 
+# Colors data file
 colors_csv = None
 
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-i","--image",required=True,
-        help="image to get its dominant color")
-
-args = vars(ap.parse_args())
-
 def getColorName(R,G,B):
+    # including colors_csv
     global colors_csv
+    # minimum variable for finding the closest color
     minimum = 1000
+    # testing colors one by one
     for i in range(len(colors_csv)):
         d = abs(R- int(colors_csv.loc[i,"R"])) + abs(G- int(colors_csv.loc[i,"G"]))+ abs(B- int(colors_csv.loc[i,"B"]))
         if(d<minimum):
@@ -24,44 +21,58 @@ def getColorName(R,G,B):
             color_name = colors_csv.loc[i,"color_name"]
     return color_name
 
-def getDominantColor(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    (thresh, im_bw) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    dilation = cv2.dilate(im_bw,(3,3),iterations = 1)
-    masked_img = cv2.bitwise_and(img,img,mask = dilation)
-    
-    pixels = np.float32(masked_img.reshape(-1, 3))
-    n_colors = 5
+def getColors(img):
+    # grayscaling and creating masked_image
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # (thresh, im_bw) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    # dilation = cv2.dilate(im_bw,(3,3),iterations = 1)
+    # masked_img = cv2.bitwise_and(img,img,mask = dilation)
+
+    # Getting dominant color with cv2.kmeans function.
+    pixels = np.float32(img.reshape(-1, 3))
+    n_colors = 2
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
     flags = cv2.KMEANS_RANDOM_CENTERS
     _, labels, palette = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
     _, counts = np.unique(labels, return_counts=True)
-    dominant = palette[np.argmax(counts)]
-    return dominant
+    print(counts)
+
+    print(palette)
+
+    # dominant = palette[np.argmax(counts)]
+    # print(dominant)
+
+    return palette
 
 def main():
     global colors_csv
     data = Dataset("./test_data/")
     images, labels = data.images, data.labels
     # temporary img
-    img = args["image"]
+    for i in range(len(images)):
+        img = images[i]
+        cv2.imshow("img", img)
+        
+
+        index=["color","color_name","hex","R","G","B"]
+        colors_csv = pd.read_csv('./colors.csv', names=index, header=None)
+
+        dominants =  getColors(img)
+        dominant_color = np.ones(img.shape, dtype=np.uint8) * np.uint8(dominants[0])
+        cv2.imshow("dc1",dominant_color)
+        dominant_color2 = np.ones(img.shape, dtype=np.uint8) * np.uint8(dominants[1])
+        cv2.imshow("dc2",dominant_color2)
 
 
-    index=["color","color_name","hex","R","G","B"]
-    colors_csv = pd.read_csv('./colors.csv', names=index, header=None)
+        r,g,b = dominants[0]
+        color_name = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
+        print(color_name)
 
-    dominant =  getDominantColor(img)
-
-
-
-    dominant_color = np.ones(img.shape, dtype=np.uint8) * np.uint8(dominant)
-    plt.imshow(dominant_color)
-    plt.show()
-
-    r,g,b = dominant
-    color_name = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-
-    print(color_name)
+        r,g,b = dominants[1]
+        color_name = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
+        print(color_name)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
--- Dataset.py
+++ Dataset.py
@@ -5,13 +5,14 @@ import cv2
 
 class Dataset:
 
-    def __init__(self, data_dir):
+    def __init__(self, data_dir,BGR = False):
         self.data_dir = data_dir
         self.classes = [os.path.basename(dir) for dir in glob.glob(self.data_dir+"/*")]
         self.length = sum(self.__imageCount().values())
         self.lengths_by_class = self.__imageCount()
         self.shape = tuple((self.length,*self.__shape()))
         self.images, self.labels = self.__create_images_and_labels()
+        self.BGR = BGR
         
         
         
@@ -32,6 +33,7 @@ class Dataset:
         Creates images from paths, creates labels from paths.
         Returns images and labels as separate arrays.
         """
+
         image_pool = []
         label_pool = []
         for subdir in self.classes:
@@ -40,7 +42,10 @@ class Dataset:
                                     glob.glob(self.data_dir + "/" +subdir +"/*.jpg") + \
                                     glob.glob(self.data_dir + "/" +subdir +"/*.jpeg")):
 
-                image = cv2.imread(path)
+                if self.BGR == False:
+                    image = cv2.imread(path,cv2.BGR2RGB)
+                else:
+                    image = cv2.imread(path)
                 image_pool.append(image)
                 label_pool.append(subdir)
                 print(f"{subdir}: {(i/self.lengths_by_class[subdir])*100:.4}",end="\r")
